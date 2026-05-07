e08c623315e097c4ab5b10702246f147e16a7ed4
xwilson03 <xanderwilson2014@gmail.com>
2026-05-06 23:49:38 -0500

    adds placeholder elements

--- lib/mainwindow/mainwindow.cpp
+++ lib/mainwindow/mainwindow.cpp
@@ -1,20 +1,21 @@
 #include "mainwindow.h"
 
+#include <QGraphicsScene>
+#include <QGraphicsView>
 #include <QHBoxLayout>
+#include <QLabel>
 #include <QVBoxLayout>
-#include <QStackedWidget>
+#include <QStackedLayout>
 
 MainWindow::MainWindow(
     QWidget* parent
 )
 : QMainWindow(parent)
 {
+    setMinimumSize(680, 480);
+    resize(800, 600);
 
-    setStyleSheet(
-        "min-width: 640px;"
-        "min-height: 480px;"
-    );
-
+    // Central Widget
     auto main_widget = new QWidget();
     setCentralWidget(main_widget);
 
@@ -22,13 +23,51 @@ MainWindow::MainWindow(
     main_layout->setContentsMargins(0, 0, 0, 0);
     main_widget->setLayout(main_layout);
 
+    // Layout
     auto topbar = new QHBoxLayout();
     main_layout->addLayout(topbar, 0);
 
-    auto page_display = new QStackedWidget();
-    page_display->setStyleSheet("background-color: white;");
-    main_layout->addWidget(page_display, 1);
+    auto page_display = new QStackedLayout();
+    main_layout->addLayout(page_display, 1);
 
-    auto main_page = new QWidget();
-    page_display->addWidget(main_page);
+    // Navigation
+    auto navbar = new QWidget();
+    topbar->addWidget(navbar, 1);
+
+    auto navbar_layout = new QHBoxLayout();
+    navbar_layout->setAlignment(Qt::AlignLeft);
+    navbar->setLayout(navbar_layout);
+
+    const auto page_names = {"Character", "Map", "Wiki", "Save & Exit"};
+    for (const auto& page_name : page_names) {
+        auto page_label = new QLabel(page_name);
+        navbar_layout->addWidget(page_label);
+    }
+
+    // Area Label
+    const auto area_name = "Gravekeeper's Tomb";
+    auto area_label = new QLabel(area_name);
+    topbar->addWidget(area_label, 0);
+
+    // Encounter Status
+    auto encounter_status = new QWidget();
+    topbar->addWidget(encounter_status, 1);
+
+    auto encounter_status_layout = new QHBoxLayout();
+    encounter_status_layout->setAlignment(Qt::AlignRight);
+    encounter_status->setLayout(encounter_status_layout);
+
+    auto encounter_stage_badge = new QLabel("Combat");
+    encounter_status_layout->addWidget(encounter_stage_badge);
+
+    auto encounter_round_badge = new QLabel("Round: 5");
+    encounter_status_layout->addWidget(encounter_round_badge);
+
+    auto encounter_turn_badge = new QLabel("Your Turn");
+    encounter_status_layout->addWidget(encounter_turn_badge);
+
+    // Content
+    auto map_graphics_scene = new QGraphicsScene();
+    auto map_page = new QGraphicsView(map_graphics_scene);
+    page_display->addWidget(map_page);
 }
