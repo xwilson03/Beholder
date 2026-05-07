0806745f325d211910f89b2c606ffc46887b126b
xwilson03 <xanderwilson2014@gmail.com>
2026-05-06 20:13:40 -0500

    adds basic layout

--- lib/mainwindow/mainwindow.cpp
+++ lib/mainwindow/mainwindow.cpp
@@ -1,9 +1,34 @@
 #include "mainwindow.h"
 
+#include <QHBoxLayout>
+#include <QVBoxLayout>
+#include <QStackedWidget>
+
 MainWindow::MainWindow(
     QWidget* parent
 )
 : QMainWindow(parent)
 {
 
+    setStyleSheet(
+        "min-width: 640px;"
+        "min-height: 480px;"
+    );
+
+    auto main_widget = new QWidget();
+    setCentralWidget(main_widget);
+
+    auto main_layout = new QVBoxLayout();
+    main_layout->setContentsMargins(0, 0, 0, 0);
+    main_widget->setLayout(main_layout);
+
+    auto topbar = new QHBoxLayout();
+    main_layout->addLayout(topbar, 0);
+
+    auto page_display = new QStackedWidget();
+    page_display->setStyleSheet("background-color: white;");
+    main_layout->addWidget(page_display, 1);
+
+    auto main_page = new QWidget();
+    page_display->addWidget(main_page);
 }
