4e1f94753e9d11ea7c6ebb1d072b2da48bc894bb
xwilson03 <xanderwilson2014@gmail.com>
2026-05-20 23:37:39 -0500

    nit: updates util scripts

---
+++ build.sh
@@ -0,0 +1,27 @@
+#!/bin/bash -eux
+
+
+while [[ $# -gt 0 ]]; do
+case $1 in
+    -c|--clean) CLEAN=; shift;;
+esac
+done
+
+REPO_DIR=$(git rev-parse --show-toplevel)
+
+SOURCE_DIR=$REPO_DIR
+BUILD_DIR=$REPO_DIR/.build
+INSTALL_DIR=$REPO_DIR/.install
+
+
+[[ -v CLEAN ]] && rm -rf $BUILD_DIR $INSTALL_DIR
+
+cmake \
+    -G Ninja                            \
+    -S $SOURCE_DIR                      \
+    -B $BUILD_DIR                       \
+    -DCMAKE_INSTALL_PREFIX=$INSTALL_DIR \
+    $SOURCE_DIR
+
+cmake --build $BUILD_DIR
+cmake --install $BUILD_DIR
--- scripts/clean.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eu
-
-HERE=$(cd $(dirname $0) && pwd)
-. $HERE/.env
-
-rm -rf           \
-    $BUILD_DIR   \
-    $INSTALL_DIR
